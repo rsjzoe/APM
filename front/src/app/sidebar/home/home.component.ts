8be314e449430eac133ead6e3b52963f12904776
rsjzoe <rsj.zoe@gmail.com>
2025-01-13 16:11:08 +0300

    feat(home): modal

--- front/src/app/sidebar/home/home.component.ts
+++ front/src/app/sidebar/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CardComponent } from "./card/card.component";
 import { applications } from '../../application-APM/data';
 import { CommonModule } from '@angular/common';
+import { ButtonComponent } from "../../components/button/button.component";
 
 @Component({
   selector: 'app-home',
-  imports: [CardComponent, CommonModule],
+  imports: [CardComponent, CommonModule, ButtonComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
