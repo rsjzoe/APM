19ebe049872ca948b6155a36b65cf71e5fb19852
rsjzoe <rsj.zoe@gmail.com>
2025-01-20 10:12:30 +0300

    components app-details

---
+++ front/src/app/app-details/app-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-details',
+  imports: [],
+  templateUrl: './app-details.component.html',
+  styleUrl: './app-details.component.scss'
+})
+export class AppDetailsComponent {
+
+}
