ee908c18211224fc52dcfc1b2bc1e409e6fea684
rsjzoe <rsj.zoe@gmail.com>
2025-04-03 16:45:53 +0300

    feat(classification): implement CRUD operations for classifications with UI enhancements

--- front/src/app/application/classe.service.ts
+++ front/src/app/application/classe.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Classe } from './classe.type';
+import { Classe, CreateClasse, UpdateClasse } from './classe.type';
 
 @Injectable({
   providedIn: 'root',
@@ -11,5 +11,21 @@ export class ClasseService {
 
   findAll() {
     return this.http.get<Classe[]>(this.apiUrl);
+  }
+
+  add(data: CreateClasse) {
+    return this.http.post<Classe>(this.apiUrl, data);
+  }
+
+  delete(id: number) {
+    return this.http.delete<Classe>(`${this.apiUrl}/${id}`);
+  }
+
+  update(id: number, data: UpdateClasse) {
+    return this.http.put<Classe>(`${this.apiUrl}/${id}`, data);
+  }
+
+  findById(id: number) {
+    return this.http.get<Classe>(`${this.apiUrl}/${id}`);
   }
 }
--- front/src/app/sidebar/classification/classification.component.ts
+++ front/src/app/sidebar/classification/classification.component.ts
@@ -1,11 +1,102 @@
 import { Component } from '@angular/core';
+import { Classe } from '../../application/classe.type';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ClasseService } from '../../application/classe.service';
 
 @Component({
   selector: 'app-classification',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './classification.component.html',
-  styleUrl: './classification.component.scss'
+  styleUrl: './classification.component.scss',
 })
 export class ClassificationComponent {
+  classes: Classe[] = [];
+  newClasse: Classe = this.getEmptyClasse();
+  editingClasse: Classe | null = null;
+  expandedId: number | null = null;
 
+  constructor(private classeService: ClasseService) {}
+
+  ngOnInit() {
+    this.findAll();
+  }
+
+  findAll() {
+    this.classeService.findAll().subscribe((classes) => {
+      this.classes = classes;
+    });
+  }
+
+  getEmptyClasse(): Classe {
+    return { id: 0, name: '', description: '', isDeleted: false };
+  }
+
+  addClass() {
+    if (this.newClasse.name.trim()) {
+      this.classeService
+        .add({
+          name: this.newClasse.name,
+          description: this.newClasse.description,
+        })
+        .subscribe((classe) => {
+          this.findAll();
+          this.newClasse = this.getEmptyClasse();
+        });
+    }
+  }
+
+  deleteClasse(id: number) {
+    this.classeService.delete(id).subscribe(() => {
+      this.findAll();
+    });
+  }
+
+  startEditing(classe: Classe) {
+    this.editingClasse = { ...classe };
+  }
+
+  updateEditingName(name: string) {
+    if (this.editingClasse) {
+      this.editingClasse = { ...this.editingClasse, name };
+    }
+  }
+
+  updateEditingDescription(description: string) {
+    if (this.editingClasse) {
+      this.editingClasse = { ...this.editingClasse, description };
+    }
+  }
+
+  saveEdit() {
+    if (!this.editingClasse) return;
+
+    this.classeService
+      .update(this.editingClasse.id, this.editingClasse)
+      .subscribe({
+        next: () => {
+          this.findAll();
+          this.editingClasse = null;
+        },
+        error: (error) => {
+          console.error('Error updating class:', error);
+        },
+      });
+  }
+
+  cancelEdit() {
+    this.editingClasse = null;
+  }
+
+  isEditing(id: number): boolean {
+    return this.editingClasse?.id === id;
+  }
+
+  toggleAccordion(id: number) {
+    this.expandedId = this.expandedId === id ? null : id;
+  }
+
+  isExpanded(id: number): boolean {
+    return this.expandedId === id;
+  }
 }
