1161c341cf1e6053da9891e372f81cd9a60416d8
rsjzoe <rsj.zoe@gmail.com>
2025-04-10 15:54:54 +0300

    feat(role): enhance RoleEntityRepository with Keycloak integration for role creation

--- api/src/main/java/org/acme/role/config/RoleConfig.java
+++ api/src/main/java/org/acme/role/config/RoleConfig.java
@@ -2,12 +2,38 @@ package org.acme.role.config;
 
 import org.acme.role.domain.port.out.RoleRepository;
 import org.acme.role.infra.database.repository.RoleEntityRepository;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import jakarta.enterprise.inject.Produces;
+import jakarta.inject.Inject;
 
 public class RoleConfig {
+    @Inject
+    @ConfigProperty(name = "keycloak.url")
+    String serverUrl;
+
+    @Inject
+    @ConfigProperty(name = "keycloak.realm")
+    String realm;
+
+    @Inject
+    @ConfigProperty(name = "keycloak.client-id")
+    String clientId;
+
+    @Inject
+    @ConfigProperty(name = "keycloak.client-secret")
+    String clientSecret;
+
+    @Inject
+    @ConfigProperty(name = "keycloak.admin.name")
+    String adminName;
+
+    @Inject
+    @ConfigProperty(name = "keycloak.admin.password")
+    String adminPassword;
+
     @Produces
     public RoleRepository roleRepository() {
-        return new RoleEntityRepository();
+        return new RoleEntityRepository(serverUrl, realm, clientId, clientSecret, adminName, adminPassword);
     }
 }
--- api/src/main/java/org/acme/role/infra/database/repository/RoleEntityRepository.java
+++ api/src/main/java/org/acme/role/infra/database/repository/RoleEntityRepository.java
@@ -7,13 +7,47 @@ import org.acme.role.domain.input.CreateRole;
 import org.acme.role.domain.model.Role;
 import org.acme.role.domain.port.out.RoleRepository;
 import org.acme.role.infra.database.entity.RoleEntity;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.admin.client.resource.RolesResource;
+import org.keycloak.representations.idm.RoleRepresentation;
 
 public class RoleEntityRepository implements RoleRepository {
+    private final String SERVER_URL;
+    private final String REALM;
+    private final String CLIENT_ID;
+    private final String CLIENT_SECRET;
+
+    private final String ADMIN_USERNAME;
+    private final String ADMIN_PASSWORD;
+
+    public RoleEntityRepository(String serverUrl, String realm, String clientId, String clientSecret,
+            String adminUsername, String adminPassword) {
+        this.SERVER_URL = serverUrl;
+        this.REALM = realm;
+        this.CLIENT_ID = clientId;
+        this.CLIENT_SECRET = clientSecret;
+        this.ADMIN_USERNAME = adminUsername;
+        this.ADMIN_PASSWORD = adminPassword;
+    }
 
     @Override
     public Role createRole(CreateRole role) {
         RoleEntity roleEntity = new RoleEntity(role);
         roleEntity.persist();
+        Keycloak keycloak = KeycloakBuilder.builder()
+                .serverUrl(SERVER_URL)
+                .realm(REALM)
+                .clientId(CLIENT_ID)
+                .clientSecret(CLIENT_SECRET)
+                .username(ADMIN_USERNAME)
+                .password(ADMIN_PASSWORD)
+                .grantType("password")
+                .build();
+        RolesResource rolesResource = keycloak.realm(REALM).roles();
+        var roleRepresentation = new RoleRepresentation();
+        roleRepresentation.setName(role.getRoleName());
+        rolesResource.create(roleRepresentation);
         return roleEntity.toRole();
     }
 
